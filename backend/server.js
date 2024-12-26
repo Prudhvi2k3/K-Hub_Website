0848b7c0805abd75ebb5f0daa4048a466602b931
Prudhvi2k3 <prudhviankamreddi1@gmail.com>
2024-12-26 20:03:12 +0530

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ const posterRoutes=require('./routes/posterroutes')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://project-k-hub.onrender.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json());
